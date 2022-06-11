83a0e2f7fff5cc5ef4a661f5c61f013420748ebe
M Umar Farooq <umarfarooq125@gmail.com>
2022-06-11 18:31:07 -0400

    Update Maximun SubArrayO3.java

--- Maximun SubArrayO3.java
+++ Maximun SubArrayO3.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
-        int sum = 0;
-        
+        int sum = Integer.MIN_VALUE;
+
         for(int i = 0 ; i < nums.length ; i++) {
             
             for(int j = i; j < nums.length ; j++) {
